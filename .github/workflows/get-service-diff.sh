06c601df322029582b956bb1796d8c4e0540e48e
Axel Ramone <axel.ramone@gmail.com>
2025-11-05 17:52:36 -0600

    chore: ignore folders and files from triggering build
    Certain files and folders like the infrastructure and README don't need
    to trigger new builds. There is nothing new about the code when only
    these files change and creating a new build would be redundant and add
    cost. If infra is provisioned AND there's a code change then a new build
    should still be triggered.
    
    [CICD]

--- .github/workflows/get-service-diff.sh
+++ .github/workflows/get-service-diff.sh
@@ -11,13 +11,20 @@ for service_dir in "${services_directories[@]}"; do
 done
 
 changed_services=()
+ignored_folders_and_files=( "infrastructure" "README.md" )
 
 for service in "${services[@]}"; do
   # Check if there are changes in the service directory
   # TODO: exclude certain directories from being considered if they are the only change
   service_in_changes=$(git diff --name-only HEAD~1 HEAD | grep -E "^services/${service}/")
   if [ -n "$service_in_changes" ]; then
-    changed_services+=("$service")
+    filtered_changes=$service_in_changes
+    for ignored in "${ignored_folders_and_files[@]}"; do
+      filtered_changes=$(echo "${filtered_changes}" | grep -vE "^services/${service}/${ignored}")
+    done
+    if [ -n "$filtered_changes" ]; then
+      changed_services+=("$service")
+    fi
   fi
 done
 
