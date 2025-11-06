5d147d1dd47b81b3590774e97e763c98bc323add
Axel Ramone <axel.ramone@gmail.com>
2025-11-05 23:26:52 -0600

    fix: temporarily allow all traffic on dev address wrapper service
    Attempting to check if I can connect to the service and make the
    GetPlaceId call. Will revert this once testing is complete.
    
    [AddressWrapper]

--- services/address_wrapper/infrastructure/terraform/main.tf
+++ services/address_wrapper/infrastructure/terraform/main.tf
@@ -8,7 +8,7 @@ resource "google_cloud_run_v2_service" "default" {
     component: "core"
   }
 
-  ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+  ingress = "INGRESS_TRAFFIC_ALL"
 
   scaling {
     min_instance_count = 0
