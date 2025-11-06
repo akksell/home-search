012a8afa5b25736fd5c839bff4977de79faabec6
Axel Ramone <axel.ramone@gmail.com>
2025-11-05 20:37:31 -0600

    chore: provision cloud run for address wrapper
    This deploys the address wrapper service to cloud run. It's only useful
    for server to server communication so it's configured to only accept
    internal traffic within the project. Additionally, a service mesh was
    not configured at this time since it adds additional uneeded complexity
    and cost. In the future, this would ideally be changed and a service
    mesh would be used.
    
    [AddressWrapper]

---
+++ services/address_wrapper/infrastructure/terraform/main.tf
@@ -0,0 +1,30 @@
+resource "google_cloud_run_v2_service" "default" {
+  name = "hs-${var.service_name}-${var.environment}"
+  location = var.region
+  description = "Service to get google place ids from an address"
+  launch_stage = "ALPHA"
+
+  labels = {
+    component: "core"
+  }
+
+  ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+
+  scaling {
+    min_instance_count = 0
+    max_instance_count = 2
+    scaling_mode = "AUTOMATIC"
+  }
+
+  template {
+    containers {
+      image = "us-south1-docker.pkg.dev/home-search-475404/homesearch-services-docker/address_wrapper:ee4379a8"
+    }
+
+    timeout = "10s"
+    health_check_disabled = false
+
+    service_account = google_service_account.default.email
+  }
+
+}
--- services/address_wrapper/infrastructure/terraform/terraform.tf
+++ services/address_wrapper/infrastructure/terraform/terraform.tf
@@ -15,3 +15,8 @@ terraform {
   }
   required_version = "1.12.0"
 }
+
+provider "google" {
+  project = var.project_id
+  region = var.region
+}
